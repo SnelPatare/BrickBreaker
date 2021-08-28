d0555379745a2a9c4493e6c45608545e41e03005
dhruti-patel <48242969+dhruti-patel@users.noreply.github.com>
2021-08-28 15:57:09 -0400

    Added ball to board

--- Ball.java
+++ Ball.java
@@ -1,5 +1,75 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
-public class Ball implements Observable {
+import javax.swing.JPanel;
+
+public class Ball extends JPanel implements Observable {
+	
+	// Box height and width
+		  int width;
+		  int height;
+		 
+		  // Ball Size
+		  float radius = 10; 
+		  float diameter = radius * 2;
+		 
+		  // Center of Call
+		  float X = radius + 50;
+		  float Y = radius + 20;
+		 
+		  // Direction
+		  float dx = 5;
+		  float dy = 5;
+		  
+		 
+		  public Ball() {
+			  
+		  }
+		  
+		  public void move() {
+			  Thread thread = new Thread() {
+			      public void run() {
+			        while (true) {
+			 
+			          width = getWidth();
+			          height = getHeight();
+			 
+			          X = X + dx ;
+			          Y = Y + dy;
+			 
+			          if (X - radius < 0) {
+			            dx = -dx; 
+			            X = radius; 
+			          } else if (X + radius > width) {
+			            dx = -dx;
+			            X = width - radius;
+			          }
+			 
+			          if (Y - radius < 0) {
+			            dy = -dy;
+			            Y = radius;
+			          } else if (Y + radius > height) {
+			            dy = -dy;
+			            Y = height - radius;
+			          }
+			          repaint();
+			 
+			          try {
+			            Thread.sleep(50);
+			          } catch (InterruptedException ex) {
+			          }
+			 
+			        }
+			      }
+			    };
+			    thread.start();
+		  }
+		 
+		  public void paintComponent(Graphics g) {
+		    super.paintComponent(g);
+		    g.setColor(Color.BLUE);
+		    g.fillOval((int)(X-radius), (int)(Y-radius), (int)diameter, (int)diameter);
+		  }
 	
 	public void addObserver(Observer o) {
 		
